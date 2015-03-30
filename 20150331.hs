872caa90866c2e28a4c6f3bb4d9dcbefea0a229d
Ana França <acff@cesar.org.br>
2015-03-30 17:42:05 -0300

    Trabalho 4 **(Incompleto)

--- 20150331.hs
+++ 20150331.hs
@@ -1,3 +1,5 @@
+
+--Q3
 type Vertice = Int
 type Aresta = (Vertice, Vertice)
 type Grafo = [Aresta]
@@ -18,4 +20,21 @@ adjacentes ((a,b):c) v
 search :: Grafo->Vertice->Vertice->[Vertice]
 search [] x y = []
   
+  
+--Q4
+baseMatriz :: [[Int]]
+baseMatriz = [[1,2,3],[4,5,6],[7,8,9]]
+
+quickSort :: (Ord x) => [x] -> [x]
+quickSort [] = []
+quickSort (h:t) = quickSort([ a|a <- t, a < h ])++(h:[])++quickSort([ a|a <- t, a >= h ])
+
+mediana :: [Int]->Int
+mediana [] = 0
+mediana list
+		|mod (length list) 2 == 0 = quickSort (list)!!((div (length list) 2)-1)
+		|otherwise = quickSort (list)!!(div (length list) 2)	
+		
+filtroMediana :: [[Int]]->Int->[Int]
+filtroMediana [[]] n = 0  
 		
